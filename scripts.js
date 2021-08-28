080cae9619099a36be35cbfa8c88ed7618589183
HaziHani <57583832+Hazihani@users.noreply.github.com>
2021-08-29 01:29:44 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,13 +3,14 @@ function startTime() {
 	let h = today.getHours();
 	let m = today.getMinutes();
 	let s = today.getSeconds();
+	h = checkTime(h);
 	m = checkTime(m);
 	s = checkTime(s);
 	document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
 	setTimeout(startTime, 1000);
-  }
+}
   
-  function checkTime(i) {
+function checkTime(i) {
 	if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
 	return i;
-  }
+}
