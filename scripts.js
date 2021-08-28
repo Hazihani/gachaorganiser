1721a0aeb1756479dd7cc78a5cdf241e537ee7ff
HaziHani <57583832+Hazihani@users.noreply.github.com>
2021-08-29 01:46:16 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,12 @@
 function startTime() {
-	const today = new Date(hours, minutes, seconds);
+	const today = new Date();
 	let h = today.getHours();
 	let m = today.getMinutes();
 	let s = today.getSeconds();
 	h = checkTime(h);
 	m = checkTime(m);
 	s = checkTime(s);
-	//document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
-	document.getElementById('txt').innerHTML =  today;
+	document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
 	setTimeout(startTime, 1000);
 }
   
