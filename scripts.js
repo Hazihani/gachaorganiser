d24b32945588fcc265493001c5189c1be8ae8e90
HaziHani <57583832+Hazihani@users.noreply.github.com>
2021-08-29 19:47:24 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -32,7 +32,7 @@ function startTime() {
 }
   
 function checkTime(i) {
-	if (i < 0) {i = 10 + i}; 
+	if (i < 0) {i = 24 + i}; 
 	if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
 	return i;
 }
