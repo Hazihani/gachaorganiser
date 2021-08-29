2337cb02b709e19c6c217b674b31d88f3eb93c3e
HaziHani <57583832+Hazihani@users.noreply.github.com>
2021-08-29 19:56:36 +0800

    time and function adjustment

--- scripts.js
+++ scripts.js
@@ -1,37 +1,37 @@
-function startTime() {
+function countdownTime() {
 	const today = new Date();
 	let h = today.getHours();
 	let m = today.getMinutes();
 	let s = today.getSeconds();
-	h = checkTime(h);
-	m = checkTime(m);
-	s = checkTime(s);
+	h = adjustTime(h);
+	m = adjustTime(m);
+	s = adjustTime(s);
 	document.getElementById('currenttime').innerHTML =  "<h2>Current Time: " + h + ":" + m + ":" + s + "</h2>";
 
 	//Dragalia Lost
-	hdl = 13 - today.getHours();
-	mdl = 60 - today.getMinutes();
-	sdl = 60 - today.getSeconds();
+	hdl = adjustTime(13 - today.getHours());
+	mdl = adjustTime(60 - today.getMinutes());
+	sdl = adjustTime(60 - today.getSeconds());
 	document.getElementById('dragalialeft').innerHTML = "Remaining Time Left: " + hdl + ":" + mdl + ":" + sdl;
 
 	//Helios: Rising  Heroes & Grand Chase
-	hhgc = 22 - today.getHours();
-	mhgc = 60 - today.getMinutes();
-	shgc = 60 - today.getSeconds();
+	hhgc = adjustTime(22 - today.getHours());
+	mhgc = adjustTime(60 - today.getMinutes());
+	shgc = adjustTime(60 - today.getSeconds());
 	hgcstring = "Remaining Time Left: " + hhgc + ":" + mhgc + ":" + shgc;
 	document.getElementById('heliosleft').innerHTML = hgcstring;
 	document.getElementById('gcleft').innerHTML = hgcstring;
 
 	//Tears of Themis
-	htot = checkTime(2 - today.getHours());
-	mtot = 60 - today.getMinutes();
-	stot = 60 - today.getSeconds();
+	htot = adjustTime(2 - today.getHours());
+	mtot = adjustTime(60 - today.getMinutes());
+	stot = adjustTime(60 - today.getSeconds());
 	document.getElementById('totleft').innerHTML = "Remaining Time Left: " + htot + ":" + mtot + ":" + stot;
 
 	setTimeout(startTime, 1000);
 }
   
-function checkTime(i) {
+function adjustTime(i) {
 	if (i < 0) {i = 24 + i}; 
 	if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
 	return i;
